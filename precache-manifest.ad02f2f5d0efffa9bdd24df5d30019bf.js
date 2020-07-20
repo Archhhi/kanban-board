aa5cb5c40a44a4c002bb172039890c9b442af210
Artur <brabus-20@mail.ru>
2020-07-20 08:35:35 +0300

    Updates

---
+++ precache-manifest.ad02f2f5d0efffa9bdd24df5d30019bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e09d03fdcc9c72e802ceab421d3a262",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "80bdd8a82a4905791e73",
+    "url": "/kanban-board/static/css/main.14733510.chunk.css"
+  },
+  {
+    "revision": "dbac0ec4f9bc45bb243a",
+    "url": "/kanban-board/static/js/2.bb0c6176.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/kanban-board/static/js/2.bb0c6176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80bdd8a82a4905791e73",
+    "url": "/kanban-board/static/js/main.8e37081f.chunk.js"
+  },
+  {
+    "revision": "151847355737b0df0a41",
+    "url": "/kanban-board/static/js/runtime-main.9173a9fd.js"
+  }
+]);
